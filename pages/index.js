6e4e7b4530f96968fafb23f679ed42e5d1ec950d
everEvergolw <1092547508@qq.com>
2024-05-22 17:22:44 +0100

    design:homepage and featrued posts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,56 @@
-function HomePage(){}
+import { Fragment } from "react";
+
+import Hero  from "../conponents/home-page/hero";
+import FeaturedPosts from "../conponents/home-page/featured-posts";
+
+const DUMMY_POSTS= [
+
+  {
+    title:'Getting Started with Nextjs', 
+    image:'getting-started-nextjs.png', 
+    excerpt: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create high-quality web applications with the power of React components.', 
+    date: '2022-02-10', 
+    slug:'getting-started-with-nextjs' },
+  {
+      title:'Getting Started with Nextjs2', 
+      image:'getting-started-nextjs.png', 
+      excerpt: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create high-quality web applications with the power of React components.', 
+      date: '2022-02-10', 
+      slug:'getting-started-with-nextjs2' },
+  {
+        title:'Getting Started with Nextjs3', 
+        image:'getting-started-nextjs.png', 
+        excerpt: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create high-quality web applications with the power of React components.', 
+        date: '2022-02-10', 
+        slug:'getting-started-with-nextjs3' },
+  {
+          title:'Getting Started with Nextjs4', 
+          image:'getting-started-nextjs.png', 
+          excerpt: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create high-quality web applications with the power of React components.', 
+          date: '2022-02-10', 
+          slug:'getting-started-with-nextjs4' }
+
+
+
+];
+
+
+function HomePage(){
+
+
+  return <Fragment> 
+
+          <Hero/>
+          <FeaturedPosts posts={DUMMY_POSTS}/>
+      </Fragment>
+
+
+}
 
 
 
 
 export default HomePage;
+
+// 1) Hero => Present ourselves
+// 2) Featured Posts
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,47 @@
-function AllPostsPage(){}
+
+import AllPosts from "../../conponents/posts/all-posts";
+
+const DUMMY_POSTS= [
+
+    {
+      title:'Getting Started with Nextjs', 
+      image:'getting-started-nextjs.png', 
+      excerpt: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create high-quality web applications with the power of React components.', 
+      date: '2022-02-10', 
+      slug:'getting-started-with-nextjs' },
+    {
+        title:'Getting Started with Nextjs2', 
+        image:'getting-started-nextjs.png', 
+        excerpt: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create high-quality web applications with the power of React components.', 
+        date: '2022-02-10', 
+        slug:'getting-started-with-nextjs2' },
+    {
+          title:'Getting Started with Nextjs3', 
+          image:'getting-started-nextjs.png', 
+          excerpt: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create high-quality web applications with the power of React components.', 
+          date: '2022-02-10', 
+          slug:'getting-started-with-nextjs3' },
+    {
+            title:'Getting Started with Nextjs4', 
+            image:'getting-started-nextjs.png', 
+            excerpt: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create high-quality web applications with the power of React components.', 
+            date: '2022-02-10', 
+            slug:'getting-started-with-nextjs4' }
+  
+  
+  
+  ];
+  
+function AllPostsPage(){
+
+    return <AllPosts posts={DUMMY_POSTS}></AllPosts>
+
+
+
+
+
+
+
+}
 
 export default AllPostsPage;
